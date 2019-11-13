c9dc0e9f81a8c9472f38977f87c6e32592cc0c68
anre <andreas.reich@unibw.de>
2019-11-13 16:40:33 +0100

    Add solid box & simplify CMakeLists.txt

---
+++ include/rviz/ogre_primitives/solid_bounding_box.h
@@ -0,0 +1,61 @@
+#ifndef SOLID_BOUNDING_BOX_H
+#define SOLID_BOUNDING_BOX_H
+
+#include <OgrePrerequisites.h>
+
+#include <OgreSimpleRenderable.h>
+
+namespace rviz
+{
+
+/** \addtogroup Core
+ *  @{
+ */
+/** \addtogroup Scene
+ *  @{
+ */
+/** Allows the rendering of a solid bounding box.
+@remarks
+    This class builds a solid renderable from a given aabb. A pointer to this class can be
+                added to a render queue to display the bounding box of an object.
+*/
+
+using namespace Ogre;
+
+class _OgreExport SolidBoundingBox : public SimpleRenderable
+{
+
+  protected:
+
+    /** Builds the solid triangle list.
+     */
+    void setupBoundingBoxVertices( const AxisAlignedBox& aab );
+
+    Real mRadius;
+
+    void _initSolidBoundingBox();
+
+  public:
+    SolidBoundingBox();
+    SolidBoundingBox( const String& name );
+    ~SolidBoundingBox();
+
+    /** Builds the solid triangle list.
+        @param
+            aabb bounding box to build a solid from.
+    */
+    void setupBoundingBox( const AxisAlignedBox& aabb );
+
+    Real getSquaredViewDepth( const Camera* cam ) const;
+
+    Real getBoundingRadius( void ) const
+    {
+        return mRadius;
+    }
+};
+/** @} */
+/** @} */
+
+} // namespace rviz
+
+#endif
---
+++ src/solid_bounding_box.cpp
@@ -0,0 +1,226 @@
+#include <OgreCamera.h>
+#include <OgreHardwareBufferManager.h>
+#include <OgreSimpleRenderable.h>
+#include <OgreTechnique.h>
+
+#include <rviz/ogre_primitives/solid_bounding_box.h>
+
+namespace rviz
+{
+#define BINDING 0
+
+SolidBoundingBox::SolidBoundingBox() : SimpleRenderable()
+{
+    _initSolidBoundingBox();
+}
+
+SolidBoundingBox::SolidBoundingBox(const String& name) : SimpleRenderable(name)
+{
+    _initSolidBoundingBox();
+}
+
+void SolidBoundingBox::_initSolidBoundingBox()
+{
+    mRenderOp.vertexData = OGRE_NEW VertexData();
+
+    mRenderOp.indexData = nullptr;
+    mRenderOp.vertexData->vertexCount = 36; // 6 faces * 2 triangles * 3 vertices = 36
+    mRenderOp.vertexData->vertexStart = 0;
+    mRenderOp.operationType = RenderOperation::OT_TRIANGLE_LIST;
+    mRenderOp.useIndexes = false;
+    mRenderOp.useGlobalInstancingVertexBufferIsAvailable = false;
+
+    VertexDeclaration* decl = mRenderOp.vertexData->vertexDeclaration;
+    VertexBufferBinding* bind = mRenderOp.vertexData->vertexBufferBinding;
+
+    unsigned long declSize = 0;
+    decl->addElement(BINDING, declSize, VET_FLOAT3, VES_POSITION);
+    declSize += VertexElement::getTypeSize(VET_FLOAT3);
+    decl->addElement(BINDING, declSize, VET_FLOAT2, VES_TEXTURE_COORDINATES);
+    declSize += VertexElement::getTypeSize(VET_FLOAT2);
+    decl->addElement(BINDING, declSize, VET_FLOAT3, VES_NORMAL);
+    declSize += VertexElement::getTypeSize(VET_FLOAT3);
+
+    HardwareVertexBufferSharedPtr vbuf = HardwareBufferManager::getSingleton().createVertexBuffer(
+        declSize, mRenderOp.vertexData->vertexCount, HardwareBuffer::HBU_STATIC_WRITE_ONLY);
+
+    // Bind buffer
+    bind->setBinding(BINDING, vbuf);
+
+    // set basic white material
+    this->setMaterial("BaseWhiteNoLighting");
+}
+
+SolidBoundingBox::~SolidBoundingBox()
+{
+    OGRE_DELETE mRenderOp.vertexData;
+}
+
+void SolidBoundingBox::setupBoundingBox(const AxisAlignedBox& aabb)
+{
+    // init the vertices to the aabb
+    setupBoundingBoxVertices(aabb);
+
+    // setup the bounding box of this SimpleRenderable
+    setBoundingBox(aabb);
+}
+
+//-----------------------------------------------------------------------
+void SolidBoundingBox::setupBoundingBoxVertices(const AxisAlignedBox& aab)
+{
+
+    Vector3 vmax = aab.getMaximum();
+    Vector3 vmin = aab.getMinimum();
+
+    Real sqLen = std::max(vmax.squaredLength(), vmin.squaredLength());
+    mRadius = Math::Sqrt(sqLen);
+
+    Real maxx = vmax.x;
+    Real maxy = vmax.y;
+    Real maxz = vmax.z;
+
+    Real minx = vmin.x;
+    Real miny = vmin.y;
+    Real minz = vmin.z;
+
+    // fill in the Vertex buffer
+    HardwareVertexBufferSharedPtr vbuf = mRenderOp.vertexData->vertexBufferBinding->getBuffer(BINDING);
+
+    auto* pPos = static_cast<float*>(vbuf->lock(HardwareBuffer::HBL_DISCARD));
+
+    // top
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = 1;
+
+    // bottom
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = minz; // position
+    *pPos++ = 1, *pPos++ = 0;                       // texture
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = -1;         // normal
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = -1;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = -1;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = -1;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = -1;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 0, *pPos++ = -1;
+
+    // back
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = 1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = 1, *pPos++ = 0, *pPos++ = 0;
+
+    // front
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = -1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = -1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = -1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = -1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = -1, *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = -1, *pPos++ = 0, *pPos++ = 0;
+
+    // left
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = maxy, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = 1, *pPos++ = 0;
+
+    // right
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = -1, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = -1, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = -1, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = minz;
+    *pPos++ = 1, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = -1, *pPos++ = 0;
+    *pPos++ = maxx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 0;
+    *pPos++ = 0, *pPos++ = -1, *pPos++ = 0;
+    *pPos++ = minx, *pPos++ = miny, *pPos++ = maxz;
+    *pPos++ = 0, *pPos++ = 1;
+    *pPos++ = 0, *pPos++ = -1, *pPos++ = 0;
+
+    vbuf->unlock();
+}
+
+//-----------------------------------------------------------------------
+Real SolidBoundingBox::getSquaredViewDepth(const Camera* cam) const
+{
+    Vector3 min, max, mid, dist;
+    min = mBox.getMinimum();
+    max = mBox.getMaximum();
+    mid = ((max - min) * 0.5) + min;
+    dist = cam->getDerivedPosition() - mid;
+
+    return dist.squaredLength();
+}
+
+}
